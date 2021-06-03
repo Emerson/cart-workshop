5626b32fb3c709a1d8fd031dfdfdbe6ff160b953
Rachel Armstrong <rachel.armstrong@imanage.com>
2021-06-03 11:50:37 -0400

    iteration 1

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,28 @@
 export default class Cart {
+  constructor() {
+    this.items = [];
+    this.total = 0;
+  }
 
+  addItem(cartObject) {
+    if (!this._validateCartObject(cartObject)) {
+      throw new Error('invalid cart object');
+    } else {
+      this.items.push(cartObject);
+      this.total = this._calculateTotal();
+    }
+  }
+  _validateCartObject(object) {
+    return true;
+  }
+
+  _calculateTotal() {
+    let total = 0;
+    this.items.forEach(item => {
+      total += item.price * item.quantity
+    });
+    return total;
+  }
 }
+
+//cart.addItem({id: "1", quantity: 2, price: 1000});
--- tests/cart.test.js
+++ tests/cart.test.js
@@ -17,4 +17,36 @@ describe("Cart", ()=> {
     let cart = new Cart();
     expect(cart).toBeTruthy();
   });
+
+  test("expect empty array on cart.items", async () => {
+    let cart = new Cart();
+    expect(cart.items).toEqual([]);
+  });
+
+  test("expect cart.total to be 0", async () => {
+    let cart = new Cart();
+    expect(cart.total).toBe(0);
+  });
+
+  test("add item method exists", async () => {
+    let cart = new Cart();
+    expect(cart.addItem).toBeTruthy();
+  });
+
+  // test("should throw error for invalid object", async () => {
+  //   let cart = new Cart();
+  //   expect(() => cart.addItem()).toThrow(Error);
+  // });
+
+  test("update item object", async () => {
+    let cart = new Cart();
+    cart.addItem({id: "1", quantity: 2, price: 1000});
+    expect(cart.items).toEqual([{id: "1", quantity: 2, price: 1000}]);
+  });
+
+  test("calculating cart total", async () => {
+    let cart = new Cart();
+    cart.addItem({id: "1", quantity: 2, price: 1000});
+    expect(cart.total).toEqual(2000);
+  });
 });
