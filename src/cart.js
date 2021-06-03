039af8b9e4f8f7880f51c118f0c5ea73f030eeb4
Emerson Lackey <emerson.lackey@imanage.com>
2021-06-03 11:33:50 -0400

    Iteration 1 tests passing

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,19 @@
 export default class Cart {
+  constructor() {
+    this._items = [];
+  }
 
+  get items() {
+    return this._items;
+  }
+
+  get total() {
+    return this.items.reduce((accumulator, item) => {
+      return accumulator + (item.price * item.quantity);
+    }, 0);
+  }
+
+  addItem(item) {
+    this._items.push(item);
+  }
 }
--- tests/cart.test.js
+++ tests/cart.test.js
@@ -17,4 +17,22 @@ describe("Cart", ()=> {
     let cart = new Cart();
     expect(cart).toBeTruthy();
   });
+
+  test("items is empty by default", async () => {
+    let cart = new Cart();
+    expect(cart.items.length).toBe(0);
+  });
+
+  test("addItem adds an item", async () => {
+    let cart = new Cart();
+    cart.addItem({id: "1", quantity: 2, price: 1000});
+    expect(cart.items[0].id).toEqual("1");
+  });
+
+  test("total returns", async () => {
+    let cart = new Cart();
+    cart.addItem({id: "1", quantity: 2, price: 1000});
+    cart.addItem({id: "2", quantity: 3, price: 1000});
+    expect(cart.total).toBe(5000);
+  });
 });
